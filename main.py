484a6766b7b0b984345282a771fdae677c533857
Qijie Zhu <qzhu9@ucsc.edu>
2021-09-30 13:08:25 -0700

    finished conversion functionality

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ equals_label = Label(text="is equal to", font=("Arial", 20), width=10)
 equals_label.grid(row=2, column=1)
 
 # Dynamic labels
-km_number_label = Label(text="0", font=("Arial", 20), width=10)
+km_number_label = Label(text="0", font=("Arial", 20), width=18)
 km_number_label.grid(row=2, column=2)
 km_number_label.config(pady=20)
 
 # User input: enter miles
-userinput_miles = Entry(font=("Arial", 20), width=10)
+userinput_miles = Entry(font=("Arial", 20), width=18)
 userinput_miles.focus()
 userinput_miles.grid(row=1, column=2)
 
@@ -28,12 +28,20 @@ userinput_miles.grid(row=1, column=2)
 # User input: calculate result
 def calculate_button_click():
     miles = userinput_miles.get()
-    # Change background to red if userinput is invalid
-    if not miles.isnumeric():
+    # Change background to red if 'miles' is invalid
+    try:
+        float(miles)
+    except ValueError:
         userinput_miles.config(bg="red")
-    # Otherwise, restore background and calculate result
     else:
+        # Restore background and calculate result
         userinput_miles.config(bg="white")
+        km = miles_to_km(float(miles))
+        km_number_label["text"] = "{0:.4f}".format(km)
+
+
+def miles_to_km(miles):
+    return miles*1.609
 
 
 calculate_button = Button(font=("Arial", 20), text="Calculate", command=calculate_button_click)
